3ef513ca945fe3bdb1ebd9ec472056778ca3f704
Joseph Weissman <joe@bignerdranch.com>
2016-04-22 17:45:33 -0400

    continue refactoring view traversal

--- lib/dedalus/view_traversal.rb
+++ lib/dedalus/view_traversal.rb
@@ -28,7 +28,7 @@ module Dedalus
         @atom_callback.call(structure, origin: origin, dimensions: dimensions) if @atom_callback
       elsif structure.is_a?(Dedalus::Element)
         # an element *other than* an atom, we need to call #show on it
-        pad = structure.padding || 2.0
+        pad = structure.padding || 0.0
         pad_origin = [x0 + pad, y0 + pad ]
         pad_dims = [width - pad*2, height - pad*2 ]
 
@@ -46,31 +46,29 @@ module Dedalus
       end
     end
 
+    private
     def walk_rows!(rows, origin:, dimensions:)
       width, height = *dimensions
       x0, y0 = *origin
 
       subdivide_line(rows, distance: height, attr: :height) do |row, current_row_height, height_cursor|
+        y = y0 + height_cursor
+        dims = [width, current_row_height]
         if row.is_a?(Array)
-          subdivide_line(row, distance: width, attr: :width) do |column, current_column_width, width_cursor|
-            if column.is_a?(Array) # we have rows INSIDE the columns! i think we need to recurse...
-              new_origin = [x0 + width_cursor,y0 + height_cursor]
-              new_dims = [ current_column_width, current_row_height ]
-              walk!(column, origin: new_origin, dimensions: new_dims)
-            else
-              x = x0 + width_cursor
-              y = y0 + height_cursor
-              walk!(column, origin: [x,y], dimensions: [ current_column_width, current_row_height ])
-
-              width_cursor += current_column_width
-            end
-          end
+          walk_columns!(row, origin: [x0, y], dimensions: dims)
         else
-          x = x0
-          y = y0 + height_cursor
-          dims = [width, current_row_height]
-          walk!(row, origin: [x,y], dimensions: dims)
+          walk!(row, origin: [x0,y], dimensions: dims)
         end
+      end
+    end
+
+    def walk_columns!(columns, origin:, dimensions:)
+      width, height = *dimensions
+      x0, y0 = *origin
+      subdivide_line(columns, distance: width, attr: :width) do |column, current_column_width, width_cursor|
+        x = x0 + width_cursor
+        dims = [ current_column_width, height ]
+        walk!(column, origin: [x,y0], dimensions: dims)
       end
     end
 
@@ -94,27 +92,26 @@ module Dedalus
       elements_with_hints = elements_with_relative_hints + elements_with_absolute_hints
       distance_specified_by_hints =
         (elements_with_relative_hints.sum(&:"#{attr}_percent") * distance) +
-        (elements_with_absolute_hints.sum(&:height))
+        (elements_with_absolute_hints.sum(&attr))
 
       default_element_section_distance = (distance - distance_specified_by_hints) / (elements.length - elements_with_hints.length)
 
       distance_cursor = 0
       elements.each_with_index do |element, index|
-        if element.is_a?(Array)
-          yield(element, default_element_section_distance, distance_cursor)
-          distance_cursor += default_element_section_distance
+        current_element_distance = if element.is_a?(Array)
+          default_element_section_distance
         else
-          current_element_distance = if !element.send(attr).nil?
-                                 element.send(attr)
-                               elsif !element.send(:"#{attr}_percent").nil?
-                                 element.send(:"#{attr}_percent") * distance
-                               else
-                                 default_element_section_distance
-                               end
-
-          yield(element, current_element_distance, distance_cursor)
-          distance_cursor += current_element_distance
+          if !element.send(attr).nil?
+            element.send(attr)
+          elsif !element.send(:"#{attr}_percent").nil?
+            element.send(:"#{attr}_percent") * distance
+          else
+            default_element_section_distance
+          end
         end
+
+        yield(element, current_element_distance, distance_cursor)
+        distance_cursor += current_element_distance
       end
     end
   end
