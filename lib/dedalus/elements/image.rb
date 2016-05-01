ecae0794ebbb7bd31bbbe310bf6cf958dd9a617d
Joseph Weissman <joe@bignerdranch.com>
2016-05-01 17:56:47 -0400

    adjust offset for inverted images

--- lib/dedalus/elements/image.rb
+++ lib/dedalus/elements/image.rb
@@ -7,7 +7,20 @@ module Dedalus
         x,y = *position
         x_scale = invert_x ? -scale : scale
         y_scale = invert_y ? -scale : scale
-        asset.draw(x + padding, y + padding, z_order, x_scale, y_scale)
+
+        ox,oy = *offset
+
+        asset.draw(x + padding + ox, y + padding + oy, z_order, x_scale, y_scale)
+      end
+
+      def offset
+        if invert_x
+          [ width, 0 ]
+        elsif invert_y
+          [ 0, height ]
+        else
+          [ 0, 0 ]
+        end
       end
 
       def z_order
@@ -35,7 +48,7 @@ module Dedalus
       end
 
       def self.example_data
-        { path: "media/images/cosmos.jpg", scale: 0.2 }
+        { path: "media/images/cosmos.jpg", scale: 0.2, invert_y: true }
       end
 
       def self.description
